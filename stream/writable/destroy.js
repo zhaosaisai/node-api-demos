37fb0a1f796c709e94f724ee394f40f3eb369d28
zhaosaisai <zhaosaisai@u51.com>
2018-06-28 13:01:20 +0800

    [update]: 添加destroy文件

--- stream/writable/destroy.js
+++ stream/writable/destroy.js
@@ -26,3 +26,69 @@ const { inherits } = require('util')
     writable.destroy()
     assert.strictEqual(writable.destroyed, true)
 }
+
+{
+    const writable = new Writable({
+        write(chunk, enc, cb) {
+            cb()
+        }
+    })
+
+    writable._name = 'second'
+    
+    const error = new Error('writable is errored')
+    writable.on('finish', () => {
+        console.log(`${writable._name} is finished`)
+    })
+
+    writable.on('close', () => {
+        console.log(`${writable._name} is closed`)
+    })
+
+    writable.on('error', err => {
+        console.log(`${writable._name} is errored`)
+        assert.strictEqual(err, error)
+        assert.strictEqual(err.message, 'writable is errored')
+    })
+
+    writable.destroy(error)
+    assert.strictEqual(writable.destroyed, true)
+}
+
+// 重写writable._destroy方法
+{
+    const writable = new Writable({
+        write(chunk, enc, cb) {
+            cb()
+        }
+    })
+    const error = new Error('writable is errored')
+
+    // 重写_destroy方法
+    writable._destroy = (err, cb) => {
+        console.log('Error in _destroy')
+        assert.strictEqual(err, error)
+        assert.strictEqual(err.message, 'writable is errored')
+        // 故意设置为null，这样就能看出这个方法被重写的作用
+        // cb(null) //如果传递的参数是null，则error事件是不会被触发的
+        cb(err)
+    }
+
+    writable._name = 'third'
+    
+    writable.on('finish', () => {
+        console.log(`${writable._name} is finished`)
+    })
+
+    writable.on('close', () => {
+        console.log(`${writable._name} is closed`)
+    })
+
+    writable.on('error', (err) => {
+        console.log(`${writable._name} is errored`)
+        assert.strictEqual(err, null)
+    })
+
+    writable.destroy(error)
+    assert.strictEqual(writable.destroyed, true)
+}
